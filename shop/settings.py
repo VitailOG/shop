e2d9538443a00195dd87d3bd39ab0d7899d0017f
Vitaliy <vzaharkiv28@gmail.com>
2021-06-12 15:18:26 +0300

    ТЕКСТ

--- shop/settings.py
+++ shop/settings.py
@@ -145,9 +145,6 @@ LOCALE_PATHS = (
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static"
-# ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -156,8 +153,8 @@ LOGIN_REDIRECT_URL = "home"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'vzaharkiv28@gmail.com'
-EMAIL_HOST_PASSWORD = 'vzakharkiv2018'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 
 
